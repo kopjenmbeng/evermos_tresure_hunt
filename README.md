a9eb1fef33a50ed17181f463259e07f15ab94270
unknown <slamet.supriyadi@bidbox.id>
2021-08-26 08:02:34 +0700

    add documentation and fixing logic

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # evermos_tresure_hunt
+
+
+This app is for Task 2: Treasure Hunt
+
+please run  command
+-   go run main.go
+
+Feel free for ask
+-   WA 087777000056
+-   email slamet.supriyadi88@gmail.com
+
+Regard
--- main.go
+++ main.go
@@ -4,7 +4,76 @@ import "fmt"
 
 func main() {
 	var treasure_posision []string
+	var obstacle []string
 
+	fmt.Println("...first grid initiate....")
+	for y := 5; y >=0; y-- {
+		for x := 0; x <= 7; x++ {
+			if y==5 || y==0{
+				fmt.Printf("%s","#")
+				o:=fmt.Sprintf("%d.%d",x,y)
+				obstacle=append(obstacle, o)
+			} else if y==3 &&( x == 2 || x==3 || x==4){
+				fmt.Printf("%s","#")
+				o:=fmt.Sprintf("%d.%d",x,y)
+				obstacle=append(obstacle, o)
+			} else if y==2 &&( x == 4 || x==6){
+				fmt.Printf("%s","#")
+				o:=fmt.Sprintf("%d.%d",x,y)
+				obstacle=append(obstacle, o)
+			} else if y==1 &&( x == 2){
+				fmt.Printf("%s","#")
+				o:=fmt.Sprintf("%d.%d",x,y)
+				obstacle=append(obstacle, o)
+			}else if y==1 &&( x == 1){
+				fmt.Printf("%s","X")
+				o:=fmt.Sprintf("%d.%d",x,y)
+				obstacle=append(obstacle, o)
+			}else {
+				if x==0 || x==7{
+					fmt.Printf("%s","#")
+					o:=fmt.Sprintf("%d.%d",x,y)
+					obstacle=append(obstacle, o)
+				}else {
+					fmt.Printf("%s",".")
+				}
+				
+			}
+			
+		}
+		fmt.Println()
+	}
+	// get probabilty treasure coordinate
+	for StartY := 1; StartY< 5; StartY++ {
+		for StartX := 2; StartX<= 7; StartX++ {
+			coor:=fmt.Sprintf("%d.%d",StartX,StartY)
+			if IsExist(coor,obstacle){
+				break
+			}else {
+				for startMinY := StartY-1; startMinY > 0; startMinY-- {
+					coor:=fmt.Sprintf("%d.%d",StartX,startMinY)
+					if IsExist(coor,obstacle){
+						break
+					}else {
+						treasure_posision=append(treasure_posision, coor)
+					}
+				}
+				// fmt.Printf("%s",pr)
+			}
+			// fmt.Println()
+
+			
+			
+		}
+		// fmt.Println()
+	}
+
+	fmt.Println("...output a list of probable coordinate points where the treasure might be located....")
+	for _,i:= range treasure_posision{
+		fmt.Println(i)
+	}
+
+	fmt.Println("...final grid where the treasure might be located....")
 	for y := 5; y >=0; y-- {
 		for x := 0; x <= 7; x++ {
 			if y==5 || y==0{
@@ -21,9 +90,12 @@ func main() {
 				if x==0 || x==7{
 					fmt.Printf("%s","#")
 				}else {
-					fmt.Printf("%s","$")
-					coor:=fmt.Sprintf("X,Y=%d,%d",x,y)
-					treasure_posision=append(treasure_posision, coor)
+					coor:=fmt.Sprintf("%d.%d",x,y)
+					if IsExist(coor,treasure_posision){
+						fmt.Printf("%s","$")
+					}else{
+						fmt.Printf("%s",".")
+					}
 				}
 				
 			}
@@ -31,9 +103,13 @@ func main() {
 		}
 		fmt.Println()
 	}
+}
 
-	fmt.Println("...output a list of probable coordinate points where the treasure might be located....")
-	for _,i:= range treasure_posision{
-		fmt.Println(i)
+func IsExist(value string,list []string)bool{
+	for _,i:= range list{
+		if i==value{
+			return true
+		}
 	}
+	return false
 }
